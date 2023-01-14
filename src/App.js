caa7b1d57dab9bf39c4295861c85c4d40ae5e547
Hunain Parekh <hunainparekh85@gmail.com>
2023-01-15 04:19:33 +0500

    final js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function login(e){
+    e.preventDefault();
+    const data = {
+      email:e.target.email.value,
+      password:e.target.password.value
+    }
+    fetch("http://localhost:8080/auth/login", {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((response) => response.json());
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +31,12 @@ function App() {
         >
           Learn React
         </a>
+        <form onSubmit={login}>
+            <input type="text" id="email"/>
+            <input type="text" id="password"/>
+            <button type='submit'>Login</button>
+        </form>
+        
       </header>
     </div>
   );
